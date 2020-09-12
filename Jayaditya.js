77be374c5c4a844d4b31f8d60f0cdac4e0759d48
Jayaditya Kakarala <37169947+jayaditya369@users.noreply.github.com>
2020-09-12 14:23:00 -0400

    Delete Jayaditya.js

--- Jayaditya.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const profile = (<div>
-    <img src="jaya.jpg" width="60%"/></div>
-);
-ReactDOM.render(profile,document.getElementById('profile'));
