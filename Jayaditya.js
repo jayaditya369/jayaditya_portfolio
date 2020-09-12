2be67d7a2de892fbad5317dd55c19065ed5e3664
Jayaditya Kakarala <37169947+jayaditya369@users.noreply.github.com>
2020-09-12 13:24:33 -0400

    Version1

---
+++ Jayaditya.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const profile = (<div>
+    <img src="jaya.jpg" width="60%"/></div>
+);
+ReactDOM.render(profile,document.getElementById('profile'));
